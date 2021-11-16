01409b4b6364392884dcca099413e0cf348c0af2
tiagobarbano <tiago.barbano@gmail.com>
2021-11-15 22:11:44 -0300

    add sql

---
+++ modelo/V1__Create_Table_Ordem.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `ordem` (
+  `id` INT(10) AUTO_INCREMENT PRIMARY KEY,
+  `tipo_transacao` enum('COMPRA','VENDA') NOT NULL,
+  `nome_ativo` enum('VIBRANIUM') NOT NULL,
+  `valor_ordem` decimal(65,2) NOT NULL,
+  `qtd_ordem` INT(10) NOT NULL,
+  `preco_medio` decimal(65,2) NOT NULL,
+  `data_ordem` datetime(6) NOT NULL,
+  `status_ordem` enum('EFETIVADA','PROCESSANDO', 'CANCELADA', 'PENDENTE') NOT NULL,  
+  `id_wallet` INT(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
